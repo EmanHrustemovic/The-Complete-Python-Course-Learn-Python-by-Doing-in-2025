2763399333db1cb57ae1e9598d60b8f00df6958f
EmanHrustemovic <eman.hrustemovic@stu.ibu.edu.ba>
2025-10-27 19:30:16 +0100

    Chapter 19 - Gui Development With Tkinter(Chapter with Projects )

--- GUI Development With Tkinter/first_app.py
+++ GUI Development With Tkinter/first_app.py
@@ -2,11 +2,21 @@ import tkinter as tk
 from tkinter import ttk
 
 def greet():
-    pass
+    print(f"Hello, {your_name.get() or 'World' }")
 
 root = tk.Tk()
 root.title('Greeter')
 
+your_name = tk.StringVar()
 
+name_label = ttk.Label(root,text="Name: ")
+name_label.pack(side="left",padx=(0,10)) #adding spacing into right side of item
+
+name_entry = ttk.Entry(root,width=15,textvariable=your_name)
+name_entry.pack(side="left")
+name_entry.focus()
+
+greet_button = ttk.Button(root,text="Greet",command=greet)
+greet_button.pack(side="left",fill="x",expand=True)
 
 root.mainloop()
