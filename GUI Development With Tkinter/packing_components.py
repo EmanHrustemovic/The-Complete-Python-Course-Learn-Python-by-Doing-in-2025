26f8c1001bb0b6db2ee218e6567942e4d875d335
EmanHrustemovic <eman.hrustemovic@stu.ibu.edu.ba>
2025-10-29 22:47:29 +0100

    Chapter 19 - Gui Development With Tkinter(Chapter with Projects )

--- GUI Development With Tkinter/packing_components.py
+++ GUI Development With Tkinter/packing_components.py
@@ -2,8 +2,11 @@ import tkinter as tk
 
 root = tk.Tk()
 
-tk.Label(root,text="Label 1",background="green").pack(side="left",fill="both",expand=True)
-tk.Label(root,text="Label 2",background="red").pack(side="top",fill="both")
+tk.Label(root,text="Label left",bg="green").pack(
+    side="left",expand=True,fill="both")
+
+tk.Label(root,text="Label red",background="red").pack(side="left",fill="both",expand=True)
+tk.Label(root,text="Label red",background="red").pack(side="top",fill="both")
 
 
 root.mainloop()
