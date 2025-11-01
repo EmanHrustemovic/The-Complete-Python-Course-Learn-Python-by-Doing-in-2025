e24d09bd8ad2924abd4fbfbf334311eaddb7a8ac
EmanHrustemovic <eman.hrustemovic@stu.ibu.edu.ba>
2025-11-01 23:15:03 +0100

    Chapter 19 - Gui Development With Tkinter(Chapter with Projects )

--- GUI Development With Tkinter/Text Editor App/app.py
+++ GUI Development With Tkinter/Text Editor App/app.py
@@ -1,6 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+def create_file():
+    text_area = tk.Text(notebook)
+    text_area.pack(fill="both", expand=True)
+    notebook.add(text_area, text="Untitled")
+    notebook.select(text_area)
+
 root = tk.Tk()
 
 main = tk.Frame(root)
@@ -9,11 +16,6 @@ main.pack(fill="both", expand=True, padx=1, pady=(4,0) )
 
 notebook = ttk.Notebook()
 notebook.pack(fill="both", expand=True)
-
-text_area = tk.Text(notebook)
-text_area.pack(fill="both", expand=True)
-notebook.add(text_area,text="Untitled")
-
-
+create_file()
 
 root.mainloop()
