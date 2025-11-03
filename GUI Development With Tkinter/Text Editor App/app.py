7b452ed4fe0bd2f67445c7612e25aa1291fab680
EmanHrustemovic <eman.hrustemovic@stu.ibu.edu.ba>
2025-11-03 22:57:30 +0100

    Chapter 19 - Gui Development With Tkinter(Chapter with Projects )

--- GUI Development With Tkinter/Text Editor App/app.py
+++ GUI Development With Tkinter/Text Editor App/app.py
@@ -2,6 +2,7 @@ import os
 import tkinter as tk
 from tkinter import ttk, filedialog
 
+text_contents = dict()
 
 def create_file(content="",title="Untitled"):
     text_area = tk.Text(notebook)
@@ -10,12 +11,31 @@ def create_file(content="",title="Untitled"):
     notebook.add(text_area, text=title)
     notebook.select(text_area)
 
+    text_contents[str(text_area)] = hash(content)
+
+def check_for_changes():
+    current = get_text_widget()
+    content = current.get("1.0","end-1c")
+    name = notebook.tab("current")["text"]
+
+    if hash(content) != text_contents[str(current)]:
+        if name[-1] != "*":
+            notebook.tab("current", text=name + "*")
+
+    elif name[-1] == "*":
+        notebook.tab("current", text=name[:-1])
+
+
+def get_text_widget():
+    text_widget = root.nametowidget(notebook.select())
+    return text_widget
+
 def save_file():
     file_path = filedialog.asksaveasfilename()
 
     try:
         filename = os.path.basename(file_path)
-        text_widget = root.nametowidget(notebook.select())
+        text_widget = get_text_widget()
         content = text_widget.get("1.0","end-1c")
 
         with open(file_path, "w") as file:
@@ -26,9 +46,10 @@ def save_file():
         return
 
     notebook.tab("current",text=filename)
+    text_contents[str(text_widget)] = hash(content)
 
 def open_file():
-    file_path = filedialog.asksaveasfilename()
+    file_path = filedialog.askopenfilename()
 
     try:
         filename = os.path.basename(file_path)
@@ -55,11 +76,17 @@ root.config(menu=menubar)
 file_menu = tk.Menu(menubar)
 menubar.add_cascade(menu=file_menu, label="File")
 
-file_menu.add_command(label="New",command=create_file)
-file_menu.add_command(label="Save",command=save_file)
+file_menu.add_command(label="New",command=create_file, accelerator="Ctrl+N")
+file_menu.add_command(label="Open...",command=open_file, accelerator="Ctrl+O")
+file_menu.add_command(label="Save",command=save_file, accelerator="Ctrl+S")
 
-notebook = ttk.Notebook()
+notebook = ttk.Notebook(main)
 notebook.pack(fill="both", expand=True)
 create_file()
+
+root.bind("<KeyPress>", lambda event : check_for_changes())
+root.bind("<Control-n>", lambda event: create_file())
+root.bind("<Control-o>", lambda event: open_file())
+root.bind("<Control-s>", lambda event: save_file())
 
 root.mainloop()
