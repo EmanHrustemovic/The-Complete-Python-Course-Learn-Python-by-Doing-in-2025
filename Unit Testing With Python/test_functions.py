3e477817a69f2866611d8528bf575c4a67e50ca8
EmanHrustemovic <eman.hrustemovic@stu.ibu.edu.ba>
2025-11-05 00:02:56 +0100

    Chapter 20 - Unit Testing With Python(Chapter with Projects )

--- Unit Testing With Python/test_functions.py
+++ Unit Testing With Python/test_functions.py
@@ -33,3 +33,27 @@ class TestFunctions(TestCase):
     def test_multiply_single_value(self):
         expected = 15
         self.assertEqual(multiply(expected),expected)
+
+    def test_multiply_zero(self):
+        expected = 0
+        self.assertEqual(multiply(expected),expected)
+
+    def test_multiply_result(self):
+        inputs = (3,5)
+        expected = 15
+        self.assertEqual(multiply(*inputs),expected)
+
+    def test_multiply_result_with_zero(self):
+        inputs = (3,5,0)
+        expected = 0
+        self.assertEqual(multiply(*inputs),expected)
+
+    def test_multiply_negative(self):
+        inputs = (3,-5,0)
+        expected = -30
+        self.assertEqual(multiply(*inputs),expected)
+
+    def test_multipy_floats(self):
+        inputs = (3.0,2)
+        expected = 6.0
+        self.assertEqual(multiply(*inputs),expected)
