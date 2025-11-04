3e6cb195ed14bfaa082e9b9bc339d4d53df98f35
EmanHrustemovic <eman.hrustemovic@stu.ibu.edu.ba>
2025-11-05 00:28:01 +0100

    Chapter 20 - Unit Testing With Python(Chapter with Projects )

--- Unit Testing With Python/test_printer.py
+++ Unit Testing With Python/test_printer.py
@@ -3,8 +3,8 @@ from .printer import Printer,PrinterError
 
 class TestPrinter(TestCase):
     @classmethod
-    def setUp(cls) -> None:
-        cls.printer = Printer(pages_per_s=2.0,capacity=300)
+    def setUp(self):
+        self.printer = Printer(pages_per_s=2.0,capacity=300)
 
     def test_print_within_capacity(self):
         message = self.printer.print(25)
