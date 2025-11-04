14808a5c5faf72323b54fc994f8ba5e60cd413ba
EmanHrustemovic <eman.hrustemovic@stu.ibu.edu.ba>
2025-11-04 17:39:52 +0100

    Chapter 20 - Unit Testing With Python(Chapter with Projects )

--- Unit Testing With Python/test_functions.py
+++ Unit Testing With Python/test_functions.py
@@ -8,3 +8,15 @@ class TestFunctions(TestCase):
         expected_result = 5.0
         self.assertAlmostEqual(divide(dividend,divisor),expected_result, delta=0.0001)
 
+    def test_divide_negative(self):
+        dividend = 15
+        divisor = -3
+        expected_result = -5.0
+        self.assertAlmostEqual(divide(dividend,divisor),expected_result,delta=0.0001)
+
+    def test_divide_dividend_zero(self):
+        dividend = 0
+        divisor = 5
+        expected_result = 0
+        self.assertEqual(divide(dividend,divisor),expected_result)
+
