ba4f0193027bd9734e9f7e9616c341a84c9513ab
EmanHrustemovic <eman.hrustemovic@stu.ibu.edu.ba>
2025-11-05 01:05:10 +0100

    Chapter 20 - Unit Testing With Python(Chapter with Projects )

--- Unit Testing With Python/test_printer.py
+++ Unit Testing With Python/test_printer.py
@@ -23,3 +23,26 @@ class TestPrinter(TestCase):
 
         result = self.printer.print(pages)
         self.assertEqual(result,expected)
+
+    def test_speed_always_two_decimals(self):
+        fast_printer = Printer(pages_per_s=3.0,capacity=300)
+        pages = 11
+        expected = "Printed 11 pages in 3.67 seconds."
+
+        result = self.printer.print(pages)
+        self.assertEqual(result,expected)
+
+    def test_multiple_print_runs(self):
+        self.printer.print(25)
+        self.printer.print(50)
+        self.printer.print(225)
+
+    def test_multiple_runs_end_up_error(self):
+        self.printer.print(25)
+        self.printer.print(50)
+        self.printer.print(225)
+
+        with self.assertRaises(PrinterError):
+            self.printer.print(1)
+
+
